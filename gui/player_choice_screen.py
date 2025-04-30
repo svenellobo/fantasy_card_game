c7e785f316096e29d57abc5e39168d73070ae531
Sven Loborec <loborecsven@gmail.com>
2025-04-30 20:20:26 +0200

    working on score calculation for player1

--- gui/player_choice_screen.py
+++ gui/player_choice_screen.py
@@ -1,10 +1,12 @@
 import customtkinter as ctk
 from gui.card_widget import CardWidget
 from constants import ALL_SUITS
+from gui.score_screen import ScoreScreen
 
 class PlayerChoiceScreen(ctk.CTkFrame):
     def __init__(self, parent, player1, player2, discard_area):
-        super().__init__(parent)        
+        super().__init__(parent) 
+        self.parent = parent       
         self.player1 = player1
         self.player2 = player2
         self.discard_area = discard_area
@@ -34,11 +36,11 @@ class PlayerChoiceScreen(ctk.CTkFrame):
         self.right_column = ctk.CTkFrame(self, fg_color="green")
         self.right_column.grid(row=0, column=2, padx=5, pady=5, sticky="nsew")
         
-        #self.player_choice_area.grid_columnconfigure(0, weight=1)
-        #self.player_choice_area.grid_columnconfigure(1, weight=1) 
-        #self.player_choice_area.grid_rowconfigure(0, weight=1)
-        #self.player_choice_area.grid_rowconfigure(1, weight=1)
-        #self.player_choice_area.grid_rowconfigure(2, weight=1)
+        self.to_score_screen_btn = ctk.CTkButton(self.right_column, fg_color="blue",
+                                                 text="To Score Screen", height=60, command=self.open_score_screen)
+        self.to_score_screen_btn.grid(row=0, column=0, padx=5, pady=5)
+        
+        
         
         self.player1_choice_lbl = ctk.CTkLabel(self.player_choice_area, text=f"Card options")
         self.player1_choice_lbl.grid(row=0, column=0, padx=5, pady=5, columnspan=7, sticky="nsew")
@@ -47,7 +49,7 @@ class PlayerChoiceScreen(ctk.CTkFrame):
         col = 0
         
         for card in self.cards_with_choice:
-            card_widget = CardWidget(self.player_choice_area, card.image, card, click_action=lambda c=card: self.player_choice(c) )
+            card_widget = CardWidget(self.player_choice_area, card.image, card, click_action=lambda c=card: self.player_choice(c))
             card_widget.grid(row=1, column=col, padx=5, pady=5, sticky="nsew")
             col += 1
             
@@ -66,7 +68,8 @@ class PlayerChoiceScreen(ctk.CTkFrame):
             row = 0
             for key,values in card.mirage_suits.items():
                 for value in values:
-                    self.btn_option = ctk.CTkButton(self.choice_area_frame, fg_color="blue", text=f"{key}: {value}", height=60)
+                    self.btn_option = ctk.CTkButton(self.choice_area_frame, fg_color="blue", text=f"{key}: {value}",
+                                                    height=60, command=lambda k=key, v=value: self.mirage_shapeshift_choice(card, k, v))
                     self.btn_option.grid(row=row, column=col, sticky="ew", padx=10, pady=5)
                     col += 1
                     if col >= 5:
@@ -78,7 +81,8 @@ class PlayerChoiceScreen(ctk.CTkFrame):
             row = 0
             for key,values in card.shape_suits.items():
                 for value in values:
-                    self.btn_option = ctk.CTkButton(self.choice_area_frame, fg_color="blue", text=f"{key}: {value}", height=60)
+                    self.btn_option = ctk.CTkButton(self.choice_area_frame, fg_color="blue", text=f"{key}: {value}",
+                                                    height=60, command=lambda k=key, v=value: self.mirage_shapeshift_choice(card, k, v))
                     self.btn_option.grid(row=row, column=col, sticky="ew", padx=10, pady=5)
                     col += 1
                     if col >= 5:
@@ -87,28 +91,50 @@ class PlayerChoiceScreen(ctk.CTkFrame):
                         
         elif card.original_state["name"] == "Doppelganger":
             col = 0
+            row = 0
             for dop_card in self.player1.cards_in_hand:
-                card_widget = CardWidget(self.choice_area_frame, card.image, dop_card)
-                card_widget.grid(row=0, column=col, padx=5, pady=5, sticky="nsew")
+                card_widget = CardWidget(self.choice_area_frame, dop_card.image, dop_card,
+                                         click_action=lambda c=card, dop_c=dop_card: self.doppelganger_choice(c, dop_c))
+                card_widget.grid(row=row, column=col, padx=5, pady=5, sticky="nsew")
                 col += 1
+                if col >= 5:
+                    col = 0
+                    row += 1
                 
         elif card.original_state["name"] == "Book of Changes":
             col = 0
+            row = 0
+            self.chosen_card = None
             for boc_card in self.player1.cards_in_hand:
-                card_widget = CardWidget(self.choice_area_frame, boc_card.image, boc_card)
-                card_widget.grid(row=0, column=col, padx=5, pady=5, sticky="nsew")
+                card_widget = CardWidget(self.choice_area_frame, boc_card.image, boc_card,
+                                         click_action=lambda b=boc_card: self.set_chosen_card(b))
+                card_widget.grid(row=row, column=col, padx=5, pady=5, sticky="nsew")
                 col += 1 
+                if col >= 5:
+                    col = 0
+                    row += 1
+                    
+            suit_col = 0
+            suit_row = 0
+            self.suits_area = ctk.CTkFrame(self.choice_area_frame, fg_color="yellow")
+            self.suits_area.grid(row=0, column=6, sticky="ew", padx=10, pady=5)
             for suit in ALL_SUITS:
-                pass
+                self.btn_option = ctk.CTkButton(self.suits_area, fg_color="blue", text=f"{suit}",
+                                                height=40, command=lambda s=suit: self.book_of_changes_choices(self.chosen_card, s))
+                self.btn_option.grid(row=suit_row, column=suit_col, sticky="ew", padx=10, pady=5)
+                suit_col += 1
+                if suit_col >= 3:
+                    suit_col = 0
+                    suit_row += 1
                 
         elif card.original_state["name"] == "Necromancer":
+            self.card_picked = False
             col = 0
             row = 0
             for disc_card in self.discard_area.discard_area_cards:
-                if disc_card.suit in card.discard_suits:
-                    print(card)
-                    print(disc_card)
-                    card_widget = CardWidget(self.choice_area_frame, disc_card.image, disc_card)
+                if disc_card.suit in card.discard_suits:                    
+                    card_widget = CardWidget(self.choice_area_frame, disc_card.image,
+                                             disc_card, click_action=self.necromancer_choice)
                     card_widget.grid(row=row, column=col, padx=5, pady=5, sticky="nsew")
                     col += 1
                     if col >= 5:
@@ -117,5 +143,36 @@ class PlayerChoiceScreen(ctk.CTkFrame):
     
             
             
+    def mirage_shapeshift_choice(self, card, suit, value):
+        card.suit = suit
+        card.name = value
+        print(card) 
+        
+    def doppelganger_choice(self, card, dop_card):
+        card.name = dop_card.name
+        card.base_power = dop_card.base_power
+        card.suit = dop_card.suit
+        print(card)
+        print(dop_card)
+        
+    def book_of_changes_choices(self, chosen_card, suit):
+        if chosen_card:
+            chosen_card.suit = suit
+            print(chosen_card)
         
         
+    def set_chosen_card(self, card):
+        self.chosen_card = card
+        print(self.chosen_card)
+        
+    def necromancer_choice(self, card):
+        if not self.card_picked:
+            self.player1.cards_in_hand.append(card)
+            self.discard_area.discard_area_cards.remove(card)
+            self.card_picked = True
+            print(card)
+            
+    def open_score_screen(self):
+        self.destroy()        
+        score_screen = ScoreScreen(self.parent, self.player1, self.player2)
+        score_screen.grid(row=0, column=0, sticky="nsew")
